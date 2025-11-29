700d9a8fc7ee6ed1ac74df44142bad1e5138f51e
mhuelsbeck2 <mhuelsbeck2@gmail.com>
2025-11-28 22:33:23 -0500

    refactor screen code

--- main.py
+++ main.py
@@ -2,14 +2,13 @@ import pygame
 from constants import SCREEN_WIDTH, SCREEN_HEIGHT
 from logger import log_state
 
-
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
 def main():
     print("Starting Asteroids with pygame version: 2.6.1")
     print(f"Screen width: {SCREEN_WIDTH}")
     print(f"Screen height: {SCREEN_HEIGHT}")
-    pygame.init()
-    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
     while True:
         log_state()
